6557f10435805fe8be867124cc2a97b887ee58b1
Nathan Cheung <nathancheung22@gmail.com>
2022-02-03 21:42:44 -0500

    initial commit

---
+++ src/main/java/User.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author student
+ */
+
+package com.mycompany.lab2exc2;
+
+import java.util.ArrayList;
+
+public class User {
+    
+    private String username;
+    private double balance;
+    private ArrayList<Book> shoppingCart;
+    
+    public User(String username, String password) {
+        this.username = username;
+        this.balance = 0.00;
+        this.shoppingCart = new ArrayList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public <any> getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void setShoppingCart(<any> shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+    
+}
